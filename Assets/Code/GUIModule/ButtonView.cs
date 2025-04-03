a972301f75d31ecbb0d71861a2cd9a840f8ec8ed
Mikhail Nemtinov <86906070+NeMikhail@users.noreply.github.com>
2025-04-03 13:26:03 +0300

    AudioAndSvingModuleImported

---
+++ Assets/Code/GUIModule/ButtonView.cs
@@ -0,0 +1,19 @@
+using GameCoreModule;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainGUI
+{
+    public class ButtonView : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private Image _buttonImage;
+        [SerializeField] private TMP_Text _buttonText;
+    
+        public Button Button => _button;
+        public Image ButtonImage => _buttonImage;
+        public TMP_Text ButtonText => _buttonText;
+    }
+}
+
