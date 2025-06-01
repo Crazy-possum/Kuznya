8ed64bcc6d486a8adda7f5e49ad9579301f5bedd
Mikhail Nemtinov <86906070+NeMikhail@users.noreply.github.com>
2025-06-01 17:38:11 +0300

    MenuSettingsCreated

---
+++ Assets/Code/TMP/MenuScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] private Slider _soundSlider;
+    [SerializeField] private Slider _musicSlider;
+    
+    
+}
